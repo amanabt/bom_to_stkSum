762d96dfa40f693672448fba5d9f3cf94d790fd5
AAT <aman.iitk072@gmail.com>
2019-06-11 12:12:31 +0530

    Units handler added. tested.

--- code/bom_filler.py
+++ code/bom_filler.py
@@ -4,6 +4,7 @@
 import csv
 import numpy as np
 import sys
+import re
 
 HEADER_SIZE = 1
 npStrDtype = '<U42'
@@ -21,7 +22,8 @@ def read_csv(filename, delimeter = ",", skip_header = 0):
     return np.transpose(np.array(content))
 
 def display_filepaths():
-
+    """
+    """
     print ("\n")
     print ("######################################################")
     print ("Stock Summary \t\t :  ", sys.argv[1])
@@ -32,14 +34,35 @@ def display_filepaths():
     print ("\n")
 
 
+def handle_units (value = ""):
+    """
+    """
+    numbers = re.findall('\d+', value)
+    #print(numbers)
+    if (len(numbers) == 1):
+        return numbers[0]
+    elif (len(numbers) == 2):
+        return numbers[0] + "." + numbers[1]
+    elif (len(numbers) == 3):
+        return numbers[0] + "." + numbers[1] + numbers[2]
+    else:
+        return
+
+    return
+
+
 display_filepaths()
 
 stkSum = read_csv(sys.argv[1])
 item_list = stkSum[:, 0]
 
 
-newStkSum = np.ndarray(shape = (len(stkSum)+1, len(sys.argv) - 1),
+
+newStkSum = np.ndarray(shape = (len(stkSum)+1, len(sys.argv) - 2),
                        dtype = npStrDtype)
+
+print("Old Stock Summary: ", stkSum.shape)
+print("New Stock Summary: ", newStkSum.shape)
 
 # Reserve rows for headers
 newStkSum[0, 0] = "Stock Item"
@@ -57,7 +80,10 @@ for bom_index, bom_name in enumerate(sys.argv[2:-1]):
     for index, item in enumerate(bom[:-7, 0]):
         if item:
             search_index = np.where(item_list == item)
-            newStkSum[search_index[0][0]+1, 1 + bom_index] = bom[index + 1, 1]
+            newStkSum[np.sum(search_index[0])+1, 1 + bom_index] = handle_units(
+                bom[index + 1, 1])
+            #value = handle_units(bom[index + 1, 1])
+            #print("->", handle_units(bom[index + 1, 1]), bom[index + 1, 1])
 
 # Exported file opens with field delimiter set to '!'
 np.savetxt(sys.argv[-1], newStkSum, delimiter="!", fmt = '%s')
